6f0343f5e36c705648add2fad16f8d75fb0bacc9
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-03-30 20:54:11 +0530

    etcd -> postgres

--- postgres_etcd/k3s/k3s_yugabyte.sh
+++ postgres_etcd/k3s/k3s_yugabyte.sh
@@ -7,3 +7,7 @@ psql -h 10.168.0.0 -p 5433 -U yugabyte
 curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode=644 \
 	--token=sample_secret_token \
 	--datastore-endpoint="postgres://yugabyte:password@10.168.0.0:5433/yugabyte"
+
+# Apply services
+k3s kubectl apply -k ./app/service.yaml
+
--- terraform/eks_managed_nodes/kms.tf
+++
@@ -1,6 +0,0 @@
-# Key Management Service
-resource "aws_kms_key" "eks" {
-  description             = "EKS Secret Encryption Key"
-  deletion_window_in_days = 10
-  enable_key_rotation     = true
-}
